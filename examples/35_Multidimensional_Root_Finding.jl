622447b56ec7ab938497513eadfbef65ce42516a
Jiahao Chen <jiahao@mit.edu>
2015-07-30 16:44:33 -0400

    Clean up example for multidimensional root finding

--- examples/35_Multidimensional_Root_Finding.jl
+++ examples/35_Multidimensional_Root_Finding.jl
@@ -1,17 +1,16 @@
+#Adapted from the GSL multiroot test
 using GSL
 
 #vector_alloc
-#v = Cdouble[1.0, 5.0, 2.0, 1.5, -1.0]
-#n = length(v)
- n = 5
- v = ccall((:gsl_vector_alloc, :libgsl), Ptr{gsl_vector}, (Csize_t,), n)
+v0 = Cdouble[1.0, 5.0, 2.0, 1.5, -1.0]
+
+n = length(v0)
+v = ccall((:gsl_vector_alloc, :libgsl), Ptr{gsl_vector}, (Csize_t,), n)
 
 # #vector_set
- ccall((:gsl_vector_set, :libgsl), Void, (Ptr{gsl_vector}, Csize_t, Cdouble), v, 0, 1.0)
- ccall((:gsl_vector_set, :libgsl), Void, (Ptr{gsl_vector}, Csize_t, Cdouble), v, 1, 5.0)
- ccall((:gsl_vector_set, :libgsl), Void, (Ptr{gsl_vector}, Csize_t, Cdouble), v, 2, 2.0)
- ccall((:gsl_vector_set, :libgsl), Void, (Ptr{gsl_vector}, Csize_t, Cdouble), v, 3, 1.5)#
- ccall((:gsl_vector_set, :libgsl), Void, (Ptr{gsl_vector}, Csize_t, Cdouble), v, 4, -1.0)
+for i=1:n
+    ccall((:gsl_vector_set, :libgsl), Void, (Ptr{gsl_vector}, Csize_t, Cdouble), v, i-1, v0[i])
+end
 
 dnewton_ptr_ptr = cglobal((:gsl_multiroot_fsolver_dnewton, :libgsl), Ptr{GSL.gsl_multiroot_fsolver_type})
 dnewton_ptr = unsafe_load(dnewton_ptr_ptr)
@@ -20,49 +19,60 @@ dnewton_solver = GSL.multiroot_fsolver_alloc(dnewton_ptr, n)
 gsl_multiroot_fsolver_set_ptr_ptr = cglobal((:gsl_multiroot_fsolver_set, :libgsl), Ptr{Ptr{GSL.gsl_multiroot_fsolver}})
 gsl_multiroot_fsolver_set_ptr = unsafe_load(gsl_multiroot_fsolver_set_ptr_ptr)
 
-@show gsl_multiroot_fsolver_set_ptr
-
-
-isdefined(:gsl_vector) || immutable gsl_vector end
-
-
 #vector_get
 for i=1:5
-    @show ccall((:gsl_vector_get, :libgsl), Cdouble, (Ptr{gsl_vector}, Csize_t), v, i-1)
+    @assert v0[i] == ccall((:gsl_vector_get, :libgsl), Cdouble, (Ptr{gsl_vector}, Csize_t), v, i-1)
 end
 
 function gsl_vector_ptr(x::Ptr{gsl_vector}, n)
-  return pointer_to_array(ccall((:gsl_vector_ptr, :libgsl), Ptr{Cdouble}, (Ptr{gsl_vector}, Csize_t), x, 0), n)
+    return pointer_to_array(ccall((:gsl_vector_ptr, :libgsl), Ptr{Cdouble}, (Ptr{gsl_vector}, Csize_t), x, 0), n)
 end
+
 function function_callback(x::Ptr{gsl_vector}, jlfunc::Function, f::Ptr{gsl_vector})
     convert(Cint, jlfunc(gsl_vector_ptr(x, n), gsl_vector_ptr(f, n)))::Cint
 end
 const function_callback_ptr = cfunction(function_callback, Cint, (Ptr{gsl_vector}, Ref{Function}, Ptr{gsl_vector}))
 
 f = function(x, f)
-  #@show x
-  f[:] = (x + (-3:2:5)) .^ 2
-  return 0
+    f[:] = (x - (-3:2:5)) .^ 2
+    0.0
 end
 
 #Ptr{multiroot_function_struct}), is the thing we want but is very hard to do in pure Julia
 #Preferable to write C shim to create the multiroot_function_struct directly.
 
 func = Ref(gsl_multiroot_function(
-  function_callback_ptr,
-  n,
-  pointer_from_objref(f)))
-@show GSL.multiroot_fsolver_set(
-  dnewton_solver,
-  Base.unsafe_convert(Ptr{eltype(func)}, func),
-  v)
-for i = 1:1000
-  status = GSL.multiroot_fsolver_iterate(dnewton_solver)
-  v = GSL.multiroot_fsolver_root(dnewton_solver)
-  if GSL.multiroot_test_residual(v, 0.01) == 0
-    println("stopped after ", i, " iterations")
-    break
-  end
+    function_callback_ptr,
+    n,
+    pointer_from_objref(f)))
+
+GSL.multiroot_fsolver_set(
+    dnewton_solver,
+    Base.unsafe_convert(Ptr{eltype(func)}, func),
+    v)
+
+maxiter = 100
+resid = 0.01
+for iter = 1:maxiter
+    status = GSL.multiroot_fsolver_iterate(dnewton_solver)
+    v = GSL.multiroot_fsolver_root(dnewton_solver)
+
+
+    #XXX What we need is to extract the residual which is a gsl_vector stored
+    #in a field called "f" in the dnewton_solver struct
+    #
+    # res = get f from dnewton_solver
+    #
+    # The rest of this test works so long as the correct residual res is given
+    #println(gsl_vector_ptr(res, n))
+    #errno = GSL.multiroot_test_residual(v, resid)
+    #if GSL.gsl_errno(errno) == GSL.SUCCESS
+    #    info("stopped after $iter iterations")
+    #    break
+    #end
 end
 v = GSL.multiroot_fsolver_root(dnewton_solver)
-println(gsl_vector_ptr(v, n))
+v1= gsl_vector_ptr(v, n)
+
+using Base.Test
+@test_approx_eq_eps v1 -3:2:5 resid
