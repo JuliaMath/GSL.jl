4340824937780c20cdaf35f00d29c9c18f461861
Jiahao Chen <jiahao@mit.edu>
2016-01-30 18:28:57 -0500

    Fix qrng_get wrapper. Closes #24

--- src/_19_2_Sampling_from_a_quasi-random_number_generator.jl
+++ src/_19_2_Sampling_from_a_quasi-random_number_generator.jl
@@ -11,10 +11,10 @@ export qrng_get
 # array x.  The space available for x must match the dimension of the
 # generator.  The point x will lie in the range 0 < x_i < 1 for each x_i.  An
 # inline version of this function is used when HAVE_INLINE is defined.
-# 
+#
 #   Returns: Cint
-function qrng_get(q::Ref{gsl_qrng}, x::Real)
+function qrng_get(q::Ref{gsl_qrng}, x::Vector{Cdouble})
     errno = ccall( (:gsl_qrng_get, libgsl), Cint, (Ref{gsl_qrng},
-        Cdouble), q, x )
+        Ref{Cdouble}), q, x )
     if errno!= 0 throw(GSL_ERROR(errno)) end
 end
