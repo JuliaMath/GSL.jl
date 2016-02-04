c3c32eddd1b4276403e8fcb60ca09a594963849f
Choi Chi Po <po@Chois-MacBook-Pro.local>
2016-02-04 03:02:08 -0800

    Fix associated Legendre and sph array

---
+++ test/7_24_2_Associated_Legendre_Polynomials_and_Spherical_Harmonics.jl
@@ -0,0 +1,14 @@
+using GSL
+using Base.Test
+
+let
+    lmax = 10
+    m = 4
+    x = -0.5
+
+    @test length(sf_legendre_Plm_array(lmax, m, x)) == lmax - m + 1
+    @test length(sf_legendre_Plm_deriv_array(lmax, m, x)) == lmax - m + 1
+    @test length(sf_legendre_sphPlm_array(lmax, m, x)) == lmax - m + 1
+    @test length(sf_legendre_sphPlm_deriv_array(lmax, m, x)) == lmax - m + 1
+
+end
