9b29b11843b7ee162b36572bd2ebd96e8e55b9fc
Choi Chi Po <po@campus-043-035.ucdavis.edu>
2016-02-04 15:37:48 -0800

    add text to runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,5 +9,6 @@ include("basic.jl")
 #XXX doesn't work anymore?
 #include("9_Permutations.jl")
 include("7_24_1_Legendre_Polynomials.jl")
+include("7_24_2_Associated_Legendre_Polynomials_and_Spherical_Harmonics.jl")
 include("28_Numerical_Differentiation.jl")
 include("HypergeometricFunctions.jl")
