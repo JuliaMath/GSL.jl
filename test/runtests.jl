f02ba902f46c839932cd571a0ae4c57294588452
lxvm <lorenzo@vanmunoz.com>
2024-01-05 21:33:47 -0800

    test Cheb

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,15 +6,15 @@ using SpecialFunctions
 Random.seed!(1)
 
 @testset "GSL" begin
-    include("error.jl")    
+    include("error.jl")
     include("hypergeom.jl")
     include("interp.jl")
     include("legendre.jl")
-    include("multidim_rootfinding.jl")    
+    include("multidim_rootfinding.jl")
     include("numdiff.jl")
     include("quadrature.jl")
-    include("rng.jl")    
+    include("rng.jl")
     include("rootfinding.jl")
     include("specfunc.jl")
+    include("chebyshev.jl")
 end
-
